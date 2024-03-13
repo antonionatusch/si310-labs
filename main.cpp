50f30477b6f77f86a834a6ba40a042650e691548
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-03-13 19:38:39 -0400

    testing local branch functionality

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Models/BinarySearchTree.h"
+#include "Models/Queues.h"
+#include "Models/Stacks.h"
+#include "Models/LinkedList.h"
+int main(){
+
+    LinkedList lista;
+    lista.InsertarAlPrincipio(5);
+    lista.Mostrar();
+
+    BinarySearchTree bst;
+    bst.Insert(5);
+    bst.Insert(4);
+    bst.InOrder();
+
+
+    return 0;
+}
