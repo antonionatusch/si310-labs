af45b6e7d805cd07851ae7baf4b199cfd28d16a4
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-04-01 12:52:03 -0400

    added all rotations of avl tree

--- Models/AvlTree.cpp
+++ Models/AvlTree.cpp
@@ -22,3 +22,69 @@ AvlNode *AvlTree::LLRotation(AvlNode *first, AvlNode *next) {
     }
     return next;
 }
+
+AvlNode *AvlTree::RRRotation(AvlNode *first, AvlNode *next) {
+    first->SetRight(next->GetLeft());
+    next->SetLeft(first);
+
+    if(next->GetBalanceFactor() == 1)
+    {
+        first->SetBalanceFactor(0);
+        next->SetBalanceFactor(0);
+    }
+    else
+    {
+        first->SetBalanceFactor(1);
+        next->SetBalanceFactor(-1);
+    }
+    return next;
+}
+
+AvlNode *AvlTree::LRRotation(AvlNode *first, AvlNode *next) {
+    AvlNode* further;
+
+    further = next->GetRight();
+    first->SetLeft(further->GetRight());
+    further->SetRight(first); // hasta aca, lo deja en rotación derecha derecha
+
+    next->SetRight(further->GetLeft());
+    further->SetLeft(next);
+
+    if(further->GetBalanceFactor() == 1)
+        next->SetBalanceFactor(-1);
+    else
+        next->SetBalanceFactor(0);
+
+    if(further->GetBalanceFactor() == -1)
+        first->SetBalanceFactor(1);
+    else
+        first->SetBalanceFactor(0);
+
+    further->SetBalanceFactor(0); // se entiende que ya está equilibrado el nodo
+    return further;
+}
+
+AvlNode *AvlTree::RLRotation(AvlNode *first, AvlNode *next) {
+    AvlNode* further;
+
+    further = next->GetLeft();
+    first->SetRight(further->GetLeft());
+    further->SetLeft(first);
+
+    next->SetLeft(further->GetRight());
+    further->SetRight(next);
+
+    if (further->GetBalanceFactor() == 1)
+        first->SetBalanceFactor(-1);
+    else
+        first->SetBalanceFactor(0);
+
+    if(further->GetBalanceFactor() == -1)
+        first->SetBalanceFactor(1);
+    else
+        first->SetBalanceFactor(0);
+    further->SetBalanceFactor(0);
+    return further;
+
+}
+
--- Models/AvlTree.h
+++ Models/AvlTree.h
@@ -3,6 +3,9 @@ class AvlTree {
 private:
     AvlTree* root;
     AvlNode* LLRotation(AvlNode* first, AvlNode* next);
+    AvlNode* RRRotation(AvlNode* first, AvlNode* next);
+    AvlNode* LRRotation(AvlNode* first, AvlNode* next);
+    AvlNode* RLRotation(AvlNode* first, AvlNode* next);
 public:
     AvlTree();
 };
