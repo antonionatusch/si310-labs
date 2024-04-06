e20f2d3dd685d246cddfa471392948ddb6b1c142
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-04-05 22:47:48 -0400

    added stupid ass useless vanilla tree shit that no one gives a fuck about

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include "Models/Stacks.h"
 #include "Models/LinkedList.h"
 #include "Models/AvlTree.h"
+#include "Models/BinaryTree.h"
 int main() {
     int n;
 
-   AvlTree avl;
+   /*AvlTree avl;
    avl.MultiInsert(5);
     avl.InOrder();
     std::cout<<"El elemento 7 se encuentra ubicado en el nodo con la siguiente dirección de memoria:"<<avl.Find(7)<<"\n";
@@ -17,8 +18,10 @@ int main() {
     std::cout<<avl.Find(7);
     std::cout<<"Altura del arbol: "<<avl.Height()<<"\n";
     std::cout<<"Es un arbol lleno? "<<avl.isFull()<<"\n";
-    std::cout<<"Cantidad de nodos: "<<avl.NodeCount()<<"\n";
-
+    std::cout<<"Cantidad de nodos: "<<avl.NodeCount()<<"\n";*/
+    std::cout<<"Arbol basico y su demo \n";
+    BinaryTree bt;
+    bt.ShortDemo();
 
     return 0;
 }
