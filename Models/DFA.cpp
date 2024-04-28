2aac3067d9141ef43a362c366fa232a524f4e906
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-04-27 23:06:18 -0400

    added new dfa declaration and readconfig functionality

--- Models/DFA.cpp
+++ Models/DFA.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "DFA.h"
+DFA::DFA() {
+}
+DFA::~DFA() {}
+
+void DFA::ReadConfig(const std::string &fileName) {
+    std::ifstream file(fileName);
+    if (!file) {
+        std::cerr << "Error al abrir el archivo de configuracion." << std::endl;
+        exit(1);
+    }
+
+    std::string token;
+    file >> token; // Leer estados
+    while (file >> token && token != "Alfabeto") {
+        this->states.push_back(token);
+    }
+
+    while (file >> token && token != "Transiciones") {
+        // Leer el alfabeto
+        for (char caracter : token) {
+            this->alphabet.push_back(caracter);
+        }
+    }
+
+    std::string originState, symbol, destinyState;
+    while (file >> originState) {
+        if (originState == "EstadoInicial") {
+            break;
+        }
+        while(file >> symbol >> destinyState){
+            this->transitions[{originState, symbol[0]}] = destinyState;
+            break;
+        }
+    }
+
+    file  >> token;
+    this->initialState = token; // El último estado leído es el estado inicial
+
+    while (file >> token && token != "EstadosAceptacion") {
+        // No hacemos nada, simplemente leemos los tokens hasta encontrar "EstadosAceptacion"
+    }
+
+    while (file >> token) {
+        this->finalStates.push_back(token);
+    }
+
+    file.close();
+}
--- Models/DFA.h
+++ Models/DFA.h
@@ -5,9 +5,29 @@
 #ifndef SI310_LABS_DFA_H
 #define SI310_LABS_DFA_H
 
+#include "iostream"
+#include "fstream"
+#include "vector"
+#include "map"
+#include "string"
 
 class DFA {
-
+private:
+    std::vector<std::string> states;
+    std::vector<char> alphabet;
+    std::map<std::pair<std::string, char>, std::string> transitions;
+    std::string initialState;
+    std::vector<std::string> finalStates;
+public:
+    explicit DFA();
+    ~DFA();
+    void AddState(const std::string& state, bool isFinal);
+    void AddTransition(const std::string& originState, char symbol, const std::string destinyState);
+    void DefineInitialState(const std::string& initialState);
+    void DefineAlphabet(const std::vector<char>& alphabet);
+    bool IsAValidChain(const std::string& chain);
+    void ReadConfig(const std::string& fileName);
+    void WriteConfig(const std::string& fileName);
 };
 
 
